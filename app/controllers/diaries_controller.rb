e910351e17ab9315b7ddbb12fd6ad11ee87f2023
KouSei089 <qq00891829@icloud.com>
2021-10-02 13:01:51 +0900

    Add:Diaryモデルと新規投稿機能の作成

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,7 +1,21 @@
 class DiariesController < ApplicationController
   def index
+    @diaries = Diary.all
   end
 
   def new
+    @diary = Diary.new
+  end
+
+  def create
+    diary = Diary.new(diary_params)
+    diary.save!
+    redirect_to root_path, notice: "筆記開示を登録しました。"
+  end
+
+  private
+
+  def diary_params
+    params.require(:diary).permit(:title, :text)
   end
 end
