183683aa66a5fc786672ffd32a4c9865315ad045
KouSei089 <qq00891829@icloud.com>
2021-10-08 17:37:07 +0900

    Add:Analysisコントローラの作成

---
+++ app/controllers/analysis_controller.rb
@@ -0,0 +1,4 @@
+class AnalysisController < ApplicationController
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'diaries#index'
   resources :diaries
+  resources :analysis
 end
