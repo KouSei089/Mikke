3884b1e8e7b644c5d1ef9acb9d4e1792fcf6859d
KouSei089 <qq00891829@icloud.com>
2021-10-08 17:35:08 +0900

    Fix:モデル名の修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_08_082749) do
+ActiveRecord::Schema.define(version: 2021_10_08_083256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "analyses", force: :cascade do |t|
+  create_table "analysis", force: :cascade do |t|
     t.string "positive_word"
     t.string "negative_word"
     t.float "emotion_point"
