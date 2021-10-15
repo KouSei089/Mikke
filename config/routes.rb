0c8aad2bee159ac6e8b5b90f994f390715e5f863
KouSei089 <qq00891829@icloud.com>
2021-10-15 11:04:20 +0900

    Add:ログイン機能一式の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'diaries#index'
   resources :diaries
   resource :analysis
+  resources :users
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'login' => "user_sessions#create"
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 end
