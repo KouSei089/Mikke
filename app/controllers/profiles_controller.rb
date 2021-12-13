5c60be07a98ca72d10852bae46e869d71f887f9e
KouSei089 <qq00891829@icloud.com>
2021-12-13 09:48:31 +0900

    他resourceのルーティングを複数形から単数形に修正しました

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,7 +6,7 @@ class ProfilesController < ApplicationController
   def update
     @user = User.find(current_user.id)
     if @user.update(user_params)
-      redirect_to settings_url, notice: '変更完了'
+      redirect_to setting_url, notice: '変更完了'
     else
       flash.now[:alert] = '漏れてます'
       render :edit
