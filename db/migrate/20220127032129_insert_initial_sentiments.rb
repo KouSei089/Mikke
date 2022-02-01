9dbe759b624e389ccce97d2b181b2e115029cf5f
KouSei089 <qq00891829@icloud.com>
2022-02-01 13:09:09 +0900

    Update:Sentimentテーブルの初期データを更新しました

--- db/migrate/20220127032129_insert_initial_sentiments.rb
+++ db/migrate/20220127032129_insert_initial_sentiments.rb
@@ -1,6 +1,15 @@
 class InsertInitialSentiments < ActiveRecord::Migration[6.0]
   def change
-    sentiments = ["嬉しい", "楽しい", "悲しい", "怒り", "恐怖"]
+    sentiments = [
+      "激怒", "怒り", "苛立ち",
+      "警戒", "予期", "関心",
+      "夢心地", "喜び", "平穏",
+      "尊い", "信頼", "容認",
+      "恐れ", "心配", "不安",
+      "驚愕", "驚き", "動揺",
+      "悲劇", "悲しみ", "感傷",
+      "憎む", "嫌悪", "退屈"
+    ]
     sentiments.each do |sentiment|
       Sentiment.create(name: sentiment)
     end
