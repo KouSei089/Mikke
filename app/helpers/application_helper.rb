1c0d0e5eec27398e5f60847485f1cd9337b6ea3f
KouSei089 <qq00891829@icloud.com>
2022-02-07 15:46:13 +0900

    Add:meta-tags(gem)を使用して、meta情報をSEO対策用に設定しました

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,33 @@ module ApplicationHelper
     content_tag(doc.to_html)
   end
 
-  def page_title(page_title = '')
-    base_title = 'Mikke（ミッケ）習慣ジャーナリング'
-    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  def default_meta_tags
+    {
+      site: 'Mikke（ミッケ）習慣ジャーナリング',
+      title: 'Mikke（ミッケ）習慣ジャーナリング',
+      reverse: true,
+      separator: '|',
+      description: 'Mikke（ミッケ）習慣ジャーナリング 書く瞑想で、新しい自分が見つかる。 ジャーナリングとは、頭に浮かぶ思考をありのまま書き出すということです。書く瞑想とも呼ばれています。Mikke（ミッケ）では、ジャーナリングをデジタルで実現し、感情の分析や習慣化しやすいようにサポートするサービスです。',
+      keywords: 'キーワード',
+      canonical: request.original_url,
+      noindex: !Rails.env.production?,
+      icon: [
+        { href: image_url('favicon.png') },
+        { href: image_url('logo.png'), rel: 'phone-icon', sizes: '180x180', type: 'image/jpg' }
+      ],
+      og: {
+        site_name: 'Mikke（ミッケ）習慣ジャーナリング',
+        title: 'Mikke（ミッケ）習慣ジャーナリング',
+        description: 'ディスクリプション',
+        type: 'website',
+        # url: request.original_url,
+        image: image_url('logo.png'),
+        locale: 'ja_JP'
+      }
+      # twitter: {
+      #   card: 'summary_large_image',
+      #   site: '@ツイッターのアカウント名',
+      # }
+    }
   end
 end
