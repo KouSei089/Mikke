4b7dae89e5750c3fceebd34197ef0243790bfd46
KouSei089 <qq00891829@icloud.com>
2022-02-21 18:25:09 +0900

    Add:パスワードをセキュアにしたことに伴い、テキストを変更しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   validates :username, presence: true
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: 'は8字以上にしてください。英数字の両方を含む必要があります' }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: 'は8文字以上にしてください。英数字の両方を含む必要があります' }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 end
