c6f01a7f501b2304600725d1b3871c8c4c61c19f
KouSei089 <qq00891829@icloud.com>
2022-02-21 18:19:17 +0900

    Add:パスワードが英数字8字以上でないとユーザー登録できないように設定しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   has_many :diaries, dependent: :destroy
   has_many :soliloquies, dependent: :destroy
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,}+\z/i.freeze
 
   authenticates_with_sorcery!
   validates :username, presence: true
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: true, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX, message: 'は8字以上にしてください。英数字の両方を含む必要があります' }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 end
