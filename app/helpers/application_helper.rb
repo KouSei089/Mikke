5a5d784ac8210d1ad1185271de4d3d4b02ddaddb
KouSei089 <qq00891829@icloud.com>
2022-02-26 19:51:19 +0900

    Update:meta情報をiphone/LINEに対応しました

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,14 +19,14 @@ module ApplicationHelper
       noindex: !Rails.env.production?,
       icon: [
         { href: image_url('favicon.png') },
-        { href: image_url('logo.png'), rel: 'phone-icon', sizes: '180x180', type: 'image/jpg' }
+        { href: image_url('logo.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' }
       ],
       og: {
         site_name: 'Mikke（ミッケ）習慣ジャーナリング',
         title: 'Mikke（ミッケ）習慣ジャーナリング',
-        description: 'ディスクリプション',
+        description: 'Mikke（ミッケ）習慣ジャーナリング 書く瞑想で、新しい自分が見つかる。 ジャーナリングとは、頭に浮かぶ思考をありのまま書き出すということです。書く瞑想とも呼ばれています。Mikke（ミッケ）では、ジャーナリングをデジタルで実現し、感情の分析や習慣化しやすいようにサポートするサービスです',
         type: 'website',
-        # url: request.original_url,
+        url: request.original_url,
         image: image_url('logo.png'),
         locale: 'ja_JP'
       }
