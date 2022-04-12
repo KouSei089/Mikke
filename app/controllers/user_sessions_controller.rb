c680a600c43892cfa575db93aea18f96899596b9
KouSei089 <qq00891829@icloud.com>
2022-04-12 19:22:05 +0900

    Clean:demo_loginアクションのコードのロジック部分をモデルに分離しました

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -19,32 +19,31 @@ class UserSessionsController < ApplicationController
   end
 
   def demo_login
-    random_value = SecureRandom.hex
-    random_value_pass = rand(11_111..99_999)
-    demo_user = User.create!(
-      username: Gimei.last.kanji,
-      email: "demo_#{random_value}@example.com",
-      password: "demou#{random_value_pass}",
-      password_confirmation: "demou#{random_value_pass}"
-    )
-    5.times do
-      Diary.create!(
-        user_id: demo_user.id,
-        title: Faker::Book.title,
-        text: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
-        creativity: rand(10..100),
-        emotion_point: rand(-1.0..1.0),
-        word_count: rand(20..600),
-        created_at: Faker::Date.backward(days: 7),
-        sentiment_ids: [rand(1..8), rand(9..16), rand(7..24)]
-      )
-      Soliloquy.create!(
-        user_id: demo_user.id,
-        text: Faker::Lorem.sentence,
-        created_at: Faker::Date.backward(days: 7)
-      )
-    end
-    auto_login(demo_user)
+    @demo_user = User.demo_user
+    create_contents
+    auto_login(@demo_user)
     redirect_to root_path, notice: t('user_sessions.flash.demo')
   end
+
+  private
+
+    def create_contents
+      5.times do
+        Diary.create!(
+          user_id: @demo_user.id,
+          title: Faker::Book.title,
+          text: Faker::Lorem.paragraph_by_chars(number: 256, supplemental: false),
+          creativity: rand(10..100),
+          emotion_point: rand(-1.0..1.0),
+          word_count: rand(20..600),
+          created_at: Faker::Date.backward(days: 7),
+          sentiment_ids: [rand(1..8), rand(9..16), rand(7..24)]
+        )
+        Soliloquy.create!(
+          user_id: @demo_user.id,
+          text: Faker::Lorem.sentence,
+          created_at: Faker::Date.backward(days: 7)
+        )
+      end
+    end
 end
